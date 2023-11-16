65388da80ade13b955eca1302e81de869d5ccd6c
levdev <149114208+underion@users.noreply.github.com>
2023-11-16 18:49:07 -0300

    Add files via upload

---
+++ pages/Chrgeur.js
@@ -0,0 +1,13 @@
+// Conteúdo básico para o arquivo Chrgeur.js
+import React from 'react';
+
+const Chrgeur = () => {
+  return (
+    <div>
+      {/* Conteúdo da sua página, se necessário */}
+      <embed src="/Chrgeur.swf" type="application/x-shockwave-flash" width="100%" height="100%" />
+    </div>
+  );
+};
+
+export default Chrgeur;
